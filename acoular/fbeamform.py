8dcf9538383762f3a1eeee9591d1d01701793d6c
Mikolaj Czuchaj <mikolaj.czuchaj@tu-berlin.de>
2025-04-03 14:25:26 +0200

    remove depreacted DictStrAny

--- acoular/fbeamform.py
+++ acoular/fbeamform.py
@@ -91,7 +91,6 @@ from traits.api import (
     Bool,
     CArray,
     Dict,
-    DictStrAny,
     Enum,
     Float,
     HasStrictTraits,
@@ -2573,7 +2572,7 @@ class BeamformerEA(BeamformerAdaptiveGrid):
     # Dictionary for setting additional keyword arguments of the
     # solver for example set the population for differential evolution
     # with kwargs['popsize'] = x
-    kwargs = DictStrAny({})
+    kwargs = Dict({})
 
     # internal identifier
     digest = Property(
